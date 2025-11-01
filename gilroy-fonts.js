91af6cf483a593dd34bf1ffb65253a70bd0d066e
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2025-11-01 15:15:51 +0300

    Delete gilroy-fonts.js

--- gilroy-fonts.js
+++
@@ -1,8 +0,0 @@
-Uncaught (in promise) File
-
-push../node_modules/pdfmake/build/pdfmake.js.A.4275.o._createDoc
-
-First 10 chars: AAEAAAATAQ
-Last 10 chars: 3N/pn+8JoA
-Contains newline: false
-Length: 222880
