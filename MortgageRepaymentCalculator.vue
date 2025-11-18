0f9fca8d86daef4d066d775002416ce8ecb36b86
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2025-11-18 21:50:48 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -2,5 +2,8 @@ npm install --save-dev sass-loader@8.0.2
 npm install --save-dev style-loader@1.3.0 --legacy-peer-deps
 npm install --save-dev postcss-loader@3.1.2 --legacy-peer-deps
 
-npm ls postcss postcss-loader
+npm error code ETARGET
+npm error notarget No matching version found for postcss-loader@3.1.2.
+npm error notarget In most cases you or one of your dependencies are requesting
+npm error notarget a package version that doesn't exist.
 
