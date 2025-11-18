031ac9cdd0d058f3a7f255f0fbc301b39b64f99f
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2025-11-18 14:17:56 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,39 +1,15 @@
 npm install --save-dev sass-loader@8.0.2
 
 
-{
-  test: /\.(sass|scss)$/i,
-  include: path.resolve(__dirname, 'src/scss'),
-  use: [
-    'cache-loader',
-    MiniCssExtractPlugin.loader,
-    {
-      loader: 'css-loader',
-      options: { sourceMap: true, url: false }
-    },
-    {
-      loader: 'postcss-loader',
-      options: {
-        ident: 'postcss',
-        sourceMap: true,
-        plugins: () => [
-          require('cssnano')({
-            preset: ['default', { discardComments: { removeAll: true } }]
-          })
-        ]
-      }
-    },
-    {
-      loader: 'sass-loader',
-      options: {
-        sourceMap: true,
-        // 👇 Ключевое: явно указываем реализацию и опции
-        implementation: require('sass'),
-        sassOptions: {
-          quietDeps: true,   // ← подавляет warnings из node_modules
-          // verbose: false, // по умолчанию
-        }
-      }
-    }
-  ]
-}
+Deprecation Warning: Sass @import rules are deprecated and will be removed in Dart Sass 3.0.0.
+
+More info and automated migrator: https://sass-lang.com/d/import
+
+  ╷
+7 │ @import "modules/index";
+  │         ^^^^^^^^^^^^^^^
+  ╵
+    stdin 7:9  root stylesheet
+
+Warning: 2 repetitive deprecation warnings omitted.
+
