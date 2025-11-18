6c75fa571bcb676b77634491373bd714d7970821
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2025-11-18 16:17:57 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,8 +1,92 @@
 npm install --save-dev sass-loader@8.0.2
 
 
-// ———— Обновляем правило SCSS dinamically ————
-  const scssRule = config.module.rules.find((rule) => rule.test && rule.test.toString().includes('sass|scss'));
+
+WARNING in ./src/js/modules/active-slide-test.js
+Module Warning (from ./node_modules/eslint-loader/index.js):
+
+C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\js\modules\active-slide-test.js
+   7:20  warning  Unexpected unnamed function  func-names
+  16:24  warning  Unexpected unnamed function  func-names
+  25:24  warning  Unexpected unnamed function  func-names
+  31:24  warning  Unexpected unnamed function  func-names
+
+✖ 4 problems (0 errors, 4 warnings)
+
+
+WARNING in ./src/js/modules/loader-test.js
+Module Warning (from ./node_modules/eslint-loader/index.js):
+
+C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\js\modules\loader-test.js
+  7:20  warning  Unexpected unnamed function  func-names
+
+✖ 1 problem (0 errors, 1 warning)
+
+
+WARNING in ./src/js/modules/mailing-page.js
+Module Warning (from ./node_modules/eslint-loader/index.js):
+
+C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\js\modules\mailing-page.js
+  241:27  warning  Unexpected unnamed function  func-names
+
+✖ 1 problem (0 errors, 1 warning)
+
+
+WARNING in ./src/js/modules/swiper-sliders.js
+Module Warning (from ./node_modules/eslint-loader/index.js):
+
+C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\js\modules\swiper-sliders.js
+  385:20  warning  Unexpected unnamed function  func-names
+  398:20  warning  Unexpected unnamed function  func-names
+
+✖ 2 problems (0 errors, 2 warnings)
+
+
+WARNING in ./src/js/modules/tabs-element.js
+Module Warning (from ./node_modules/eslint-loader/index.js):
+
+C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\js\modules\tabs-element.js
+  8:28  warning  Unexpected unnamed function  func-names
+
+✖ 1 problem (0 errors, 1 warning)
+
+
+WARNING in ./src/js/modules/test-pop-up.js
+Module Warning (from ./node_modules/eslint-loader/index.js):
+
+C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\js\modules\test-pop-up.js
+  75:32  warning  Unexpected unnamed function  func-names
+  78:19  warning  Unexpected unnamed function  func-names
+  81:21  warning  Unexpected unnamed function  func-names
+
+✖ 3 problems (0 errors, 3 warnings)
+
+
+WARNING in ./src/js/modules/video/play-video.js
+Module Warning (from ./node_modules/eslint-loader/index.js):
+
+C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\js\modules\video\play-video.js
+  56:60  warning  Unexpected unnamed function  func-names
+
+✖ 1 problem (0 errors, 1 warning)
+
+
+WARNING in ./src/js/modules/video/video-with-preview.js
+Module Warning (from ./node_modules/eslint-loader/index.js):
+
+C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\js\modules\video\video-with-preview.js
+  6:20  warning  Unexpected unnamed function  func-names
+
+✖ 1 problem (0 errors, 1 warning)
+
+
+ERROR in Entry module not found: Error: Can't resolve 'style-loader' in 'C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf'
+
+ERROR in multi (webpack)-dev-server/client?http://localhost:8080 (webpack)/hot/dev-server.js ./src/scss/style.scss
+Module not found: Error: Can't resolve 'style-loader' in 'C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf'
+i ｢wdm｣: Failed to compile.
+Error from chokidar (C:\): Error: EBUSY: resource busy or locked, lstat 'C:\pagefile.sys'
+
 
 
 
