2ead3414265c57826febbe7332e1a26a8c3a9927
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2025-11-18 14:35:11 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,15 +1,9 @@
 npm install --save-dev sass-loader@8.0.2
 
 
-{
-  loader: 'sass-loader',
-  options: {
-    sourceMap: true,
-    implementation: require('sass'),
-    sassOptions: {
-      quietDeps: true,
-      silenceDeprecations: ['division-by-slash', 'import']
-    }
-  }
-}
+ The legacy JS API is deprecated and will be removed in Dart Sass 2.0.0.
+Warning: Invalid deprecation "division-by-slash".
+
+Deprecation Warning: The legacy JS API is deprecated and will be removed in Dart Sass 2.0.0.
+
 
