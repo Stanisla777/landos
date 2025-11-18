2f1850216a3c9ca652a4e3c0a83bacbd7a439c37
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2025-11-18 15:04:35 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,29 +1,8 @@
 npm install --save-dev sass-loader@8.0.2
 
 
-{
-  enforce: 'pre',
-  test: /\.js$/,
-  exclude: /node_modules/,
-  loader: 'eslint-loader',
-  options: {
-    // cache: true  // можно раскомментировать, если хотите кэш eslint
-  }
-},
-{
-  test: /\.js$/,
-  exclude: /node_modules/,
-  use: [
-    'cache-loader',
-    {
-      loader: 'babel-loader',
-      options: {
-        cacheDirectory: true  // ← включает кэш внутри babel-loader
-      }
-    },
-    'source-map-loader'
-  ]
-}
+Browserslist: browsers data (caniuse-lite) is 13 months old. Please run:
+  npx update-browserslist-db@latest
 
 
 
