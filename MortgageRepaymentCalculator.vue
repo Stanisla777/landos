a1b8b2a70604f21f94fbc336a98b0000f97241b9
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2025-11-18 21:28:37 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,11 +1,15 @@
 npm install --save-dev sass-loader@8.0.2
 npm install --save-dev style-loader@1.3.0 --legacy-peer-deps
 
-{
-  loader: 'postcss-loader',
-  options: {
-    ident: 'postcss',      // ← ОБЯЗАТЕЛЬНО для v3
-    sourceMap: !isProduction
-    // plugins — НЕ нужно, берётся из postcss.config.js
-  }
+× ｢wds｣:  Error: listen EADDRINUSE: address already in use 127.0.0.1:8080
+    at Server.setupListenHandle [as _listen2] (node:net:1817:16)
+    at listenInCluster (node:net:1865:12)
+    at GetAddrInfoReqWrap.doListen [as callback] (node:net:2014:7)
+    at GetAddrInfoReqWrap.onlookup [as oncomplete] (node:dns:109:8) {
+  code: 'EADDRINUSE',
+  errno: -4091,
+  syscall: 'listen',
+  address: '127.0.0.1',
+  port: 8080
 }
+
