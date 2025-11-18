8e542ded0d6b1f5c68b3731cac951059f0c4b4b9
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2025-11-18 22:49:47 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -3,93 +3,14 @@ npm install --save-dev style-loader@1.3.0 --legacy-peer-deps
 npm install --save-dev postcss@7 --legacy-peer-deps
 
 
-npm ls postcss postcss-loader cssnano
+{
+  "overrides": {
+    "postcss": "7.0.39"
+  }
+}
 
-+-- autoprefixer@9.8.8
-| `-- postcss@7.0.39 deduped
-+-- css-loader@3.6.0
-| +-- icss-utils@4.1.1
-| | `-- postcss@7.0.39 deduped
-| +-- postcss-modules-extract-imports@2.0.0
-| | `-- postcss@7.0.39 deduped
-| +-- postcss-modules-local-by-default@3.0.3
-| | `-- postcss@7.0.39 deduped
-| +-- postcss-modules-scope@2.2.0
-| | `-- postcss@7.0.39 deduped
-| +-- postcss-modules-values@3.0.0
-| | `-- postcss@7.0.39 deduped
-| `-- postcss@7.0.39 deduped
-+-- cssnano@4.1.11
-| +-- cssnano-preset-default@4.0.8
-| | +-- css-declaration-sorter@4.0.1
-| | | `-- postcss@7.0.39 deduped
-| | +-- cssnano-util-raw-cache@4.0.1
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-calc@7.0.5
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-colormin@4.0.3
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-convert-values@4.0.1
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-discard-comments@4.0.2
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-discard-duplicates@4.0.2
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-discard-empty@4.0.1
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-discard-overridden@4.0.1
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-merge-longhand@4.0.11
-| | | +-- postcss@7.0.39 deduped
-| | | `-- stylehacks@4.0.3
-| | |   `-- postcss@7.0.39 deduped
-| | +-- postcss-merge-rules@4.0.3
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-minify-font-values@4.0.2
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-minify-gradients@4.0.2
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-minify-params@4.0.2
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-minify-selectors@4.0.2
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-normalize-charset@4.0.1
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-normalize-display-values@4.0.2
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-normalize-positions@4.0.2
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-normalize-repeat-style@4.0.2
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-normalize-string@4.0.2
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-normalize-timing-functions@4.0.2
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-normalize-unicode@4.0.1
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-normalize-url@4.0.1
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-normalize-whitespace@4.0.2
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-ordered-values@4.1.2
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-reduce-initial@4.0.3
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-reduce-transforms@4.0.2
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-svgo@4.0.3
-| | | `-- postcss@7.0.39 deduped
-| | +-- postcss-unique-selectors@4.0.1
-| | | `-- postcss@7.0.39 deduped
-| | `-- postcss@7.0.39 deduped
-| `-- postcss@7.0.39 deduped
-+-- postcss-loader@3.0.0
-| `-- postcss@7.0.39 deduped
-+-- postcss@7.0.39
-+-- vue-loader@15.11.1
-| `-- @vue/component-compiler-utils@3.3.0
-|   `-- postcss@7.0.39 deduped
-`-- vue@2.7.16
-  `-- @vue/compiler-sfc@2.7.16
-    `-- postcss@8.4.47
+npm install --package-lock-only
+npm install
 
+
+npm ls postcss
