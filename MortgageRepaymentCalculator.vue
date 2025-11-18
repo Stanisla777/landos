7a96c66a3817d5ca02573b6ae2bc74005125536f
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2025-11-18 20:35:07 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,21 +1,12 @@
 npm install --save-dev sass-loader@8.0.2
 npm install --save-dev style-loader@1.3.0 --legacy-peer-deps
 
+You did not set any plugins, parser, or stringifier. Right now, PostCSS does nothing. Pick plugins for your case on https://www.postcss.parts/ and use them in postcss.config.js.
 
-// В самом начале webpack.config.js, после импортов:
-const cssnano = require('cssnano'); // ← добавьте эту строку
 
-{
-  loader: 'postcss-loader',
-  options: {
-    ident: 'postcss',
-    sourceMap: !isProduction,
-    plugins: isProduction
-      ? [
-          cssnano({
-            preset: ['default', { discardComments: { removeAll: true } }]
-          })
-        ]
-      : []
-  }
-}
+WARNING in ./src/js/modules/video/play-video.js
+Module Warning (from ./node_modules/eslint-loader/index.js):
+
+C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\js\modules\video\play-video.js
+  56:60  warning  Unexpected unnamed function  func-names
+
