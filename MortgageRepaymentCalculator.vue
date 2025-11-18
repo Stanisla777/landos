6d56cbf2aff4d13bc5b770a275670d3f94422dd1
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2025-11-19 00:09:31 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -9,3 +9,15 @@ npm error notarget No matching version found for @vkid/captcha@^1.0.4.
 npm error notarget In most cases you or one of your dependencies are requesting
 npm error notarget a package version that doesn't exist.
 
+
+npm notice
+npm notice New major version of npm available! 10.7.0 -> 11.6.2
+npm notice Changelog: https://github.com/npm/cli/releases/tag/v11.6.2
+npm notice To update run: npm install -g npm@11.6.2
+npm notice
+start-webapck-template@2.0.0 C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf
++-- css-loader@3.6.0
+`-- vue-loader@15.11.1
+  `-- css-loader@3.6.0 deduped
+
+
