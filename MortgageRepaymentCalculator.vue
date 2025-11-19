c7e08f16e72061f8588feb927d17aa0c0a9768b8
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2025-11-19 18:40:15 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -3,6 +3,7 @@ npm install --save-dev style-loader@1.3.0 --legacy-peer-deps
 npm install --save-dev postcss@7 --legacy-peer-deps
 npm install --save-dev css-loader@4.3.0 --legacy-peer-deps
 npm install --save-dev cross-env
+npm install concurrently@8.2.2 --save-dev
 
 
 2092 modules
