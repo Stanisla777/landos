4c8c1d2e8f536679f29deb73379575ab988ebb80
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2025-11-19 12:29:45 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -11,30 +11,4 @@ i ｢wdm｣: Compiled successfully.
 ----------------------------------------------------------
 
 
-C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\vue-loader\lib\index.js:26
-    loaderContext.emitError(
-                  ^
-
-TypeError: loaderContext.emitError is not a function
-    at new module.exports (C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\vue-loader\lib\index.js:26:19)
-    at module.exports (C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\webpack.config.js:188:7)
-    at handleFunction (C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\webpack-cli\bin\utils\prepareOptions.js:21:13)
-    at prepareOptions (C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\webpack-cli\bin\utils\prepareOptions.js:9:5)
-    at requireConfig (C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\webpack-cli\bin\utils\convert-argv.js:117:14)
-    at C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\webpack-cli\bin\utils\convert-argv.js:123:17
-    at Array.forEach (<anonymous>)
-    at module.exports (C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\webpack-cli\bin\utils\convert-argv.js:121:15)
-    at Object.<anonymous> (C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\webpack-dev-server\bin\webpack-dev-server.js:84:40)
-    at Module._compile (node:internal/modules/cjs/loader:1364:14)
-    at Module._extensions..js (node:internal/modules/cjs/loader:1422:10)
-    at Module.load (node:internal/modules/cjs/loader:1203:32)
-    at Module._load (node:internal/modules/cjs/loader:1019:12)
-    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:128:12)
-    at node:internal/main/run_main_module:28:49
-
-Node.js v18.20.4
-npm notice
-npm notice New major version of npm available! 10.7.0 -> 11.6.2
-npm notice Changelog: https://github.com/npm/cli/releases/tag/v11.6.2
-npm notice To update run: npm install -g npm@11.6.2
-npm notice
+npm install --save-dev vue-loader@15.9.8 vue-template-compiler@2.6.14 copy-webpack-plugin@6.4.1 html-webpack-plugin@4.5.2 webpack-dev-server@3.11.3
