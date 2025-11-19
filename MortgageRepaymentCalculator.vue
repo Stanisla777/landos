b1a328713ff0478d15d9c6982f2660f32b28e35e
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2025-11-19 12:31:11 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -12,3 +12,15 @@ i ｢wdm｣: Compiled successfully.
 
 
 npm install --save-dev vue-loader@15.9.8 vue-template-compiler@2.6.14 copy-webpack-plugin@6.4.1 html-webpack-plugin@4.5.2 webpack-dev-server@3.11.3
+
+(Use `node --trace-warnings ...` to show where the warning was created)
+npm notice
+npm notice New major version of npm available! 10.7.0 -> 11.6.2
+npm notice Changelog: https://github.com/npm/cli/releases/tag/v11.6.2
+npm notice To update run: npm install -g npm@11.6.2
+npm notice
+npm error code ETARGET
+npm error notarget No matching version found for @vkid/captcha@^1.0.4.
+npm error notarget In most cases you or one of your dependencies are requesting
+npm error notarget a package version that doesn't exist.
+
