8bbd49882e5ee3f7f1026d8f6c00dfd7692cb738
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2025-11-19 10:00:30 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -8,7 +8,12 @@ npm install --save-dev css-loader@4.3.0 --legacy-peer-deps
 i ｢wdm｣: Compiled successfully.
 
 
-Updated modules:
+App hot update...
+log.js:24 [HMR] Checking for updates on the server...
+VM4842 log.js:24 [HMR] Nothing hot updated.
+VM4842 log.js:24 [HMR] App is up to date.
+log.js:24 [HMR] Updated modules:
 log.js:16 [HMR]  - ./src/scss/style.scss
+log.js:24 [HMR] App is up to date.
 
 You did not set any plugins, parser, or stringifier. Right now, PostCSS does nothing. Pick plugins for your case on https://www.postcss.parts/ and use them in postcss.config.js.
