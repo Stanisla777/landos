22d63ae22adadbb79eab520d702230110f750b67
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2026-03-05 19:08:24 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,4 +1,8 @@
-ul.regulations-act
-    each val in [1,2,3,4,5,6,7,8,9,10]
-        li.regulations-act__item
-            a(href="#") Федеральный закон
+HTML parse error: Invalid first character of tag name
+     ╭─[137:53]
+ 135 │                                                 p.personal-office__user-table-wr 25 000
+ 136 │ 
+ 137 │                                         while count < 10
+     ·                                                      ─
+ 138 │                                             .personal-office__table
+ 139 │                                                 .personal-office__table-item
