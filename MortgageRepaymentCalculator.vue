b31d543c1586a685e911b12d5a014ef52da491c8
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2026-03-05 22:10:01 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,15 +1,1 @@
-extends ../templates/base
-
-block title
-    title Инструкция
-    meta(name="description", content="Описание страницы")
-
-block content
-    .pages-wrap
-        .pages-content.js--page-content
-            include ../includes/header-personal-account
-            +header(0)
-            main
-                .container
-                    .block
-                        p Привет
+npx pug src/pug/views/test.pug --out dist-test
