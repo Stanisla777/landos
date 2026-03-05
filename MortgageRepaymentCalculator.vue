25fcaab1ee32db74a6f8af314507c683fa505d56
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2026-03-05 16:37:02 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,119 +1,16 @@
-const { parse, compileTemplate, compileScript, rewriteDefault } = require('@vue/compiler-sfc');
-const crypto = require('crypto');
-
-module.exports = function vueCompilerLoader(source) {
-  const callback = this.async();
-  const filename = this.resourcePath;
-  const id = crypto.createHash('md5').update(filename).digest('hex').slice(0, 8);
-
-  console.log('🔍 [VUE LOADER] Compiling:', filename);
-
-  try {
-    const { descriptor, errors } = parse(source, { filename });
-
-    if (errors && errors.length > 0) {
-      callback(new Error(errors[0].message));
-      return;
-    }
-
-    // Компилируем script
-    let scriptContent = '';
-    let bindings;
-    
-    if (descriptor.script) {
-      const compiledScript = compileScript(descriptor, { id });
-      scriptContent = rewriteDefault(compiledScript.content, '__component');
-      bindings = compiledScript.bindings;
-    } else {
-      scriptContent = 'const __component = {};\n';
-    }
-
-    // Компилируем template
-    let templateRender = '';
-    if (descriptor.template) {
-      const templateResult = compileTemplate({
-        source: descriptor.template.content,
-        filename,
-        id,
-        compilerOptions: {
-          scopeId: descriptor.styles.some(s => s.scoped) ? `data-v-${id}` : null,
-          bindingMetadata: bindings
-        }
-      });
-
-      if (templateResult.errors.length) {
-        callback(new Error(templateResult.errors[0].message));
-        return;
-      }
-
-      templateRender = templateResult.code.replace('export function render', 'function render');
-    }
-
-    // Обрабатываем стили внутри компонента
-    let stylesCode = '';
-    if (descriptor.styles.length > 0) {
-      stylesCode = descriptor.styles.map((style, index) => {
-        const styleId = style.scoped ? `data-v-${id}` : '';
-        const styleContent = JSON.stringify(style.content);
-        
-        // Добавляем инлайн стили в head
-        return `
-          (function() {
-            const styleId = 'vue-style-${id}-${index}';
-            // Проверяем, не добавлен ли уже этот стиль
-            if (!document.getElementById(styleId)) {
-              const styleElement = document.createElement('style');
-              styleElement.id = styleId;
-              styleElement.innerHTML = ${styleContent};
-              ${styleId ? `styleElement.setAttribute('scoped', '${styleId}');` : ''}
-              styleElement.setAttribute('data-vue-component', '${id}');
-              document.head.appendChild(styleElement);
-              
-              // Для HMR - удаляем старый стиль при обновлении
-              if (import.meta.hot) {
-                import.meta.hot.dispose(() => {
-                  const oldStyle = document.getElementById(styleId);
-                  if (oldStyle) oldStyle.remove();
-                });
-              }
-            }
-          })();
-        `;
-      }).join('\n');
-    }
-
-    // Определяем, какие импорты из vue нужны
-    const vueImports = ['defineComponent'];
-    if (templateRender) {
-      vueImports.push('resolveComponent', 'openBlock', 'createElementBlock', 'createVNode');
-      // Проверяем, используется ли render функция
-      if (templateRender.includes('render')) {
-        vueImports.push('render');
-      }
-    }
-
-    // Финальный код
-    const output = `
-      import { ${vueImports.join(', ')} } from 'vue';
-
-      ${scriptContent}
-
-      ${templateRender || ''}
-
-      ${stylesCode}
-
-      const __component__ = defineComponent({
-        ...__component,
-        name: __component.name || 'AnonymousComponent',
-        ${templateRender ? 'render' : ''}
-      });
-
-      export default __component__;
-    `;
-
-    callback(null, output);
-  } catch (err) {
-    console.error('❌ [VUE LOADER] Error:', err);
-    callback(err);
-  }
-};
+[webpack-dev-middleware] [Error:   × TypeError: Cannot read properties of undefined (reading 'getPublicPath')
+<e>   │     at HtmlWebpackPlugin.htmlWebpackPluginAssets (C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\html-webpack-plugin\index.js:411:34)
+<e>   │     at C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\html-webpack-plugin\index.js:122:27
+<e>   │     at Object.fn (C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\@rspack\core\dist\index.js:10390:21)
+<e>   │     at next (C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\@rspack\lite-tapable\dist\index.cjs:448:50)
+<e>   │     at AsyncSeriesHook.callAsyncStageRange (C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\@rspack\lite-tapable\dist\index.cjs:475:9)
+<e>   │     at C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\@rspack\lite-tapable\dist\index.cjs:105:18
+<e>   │     at new Promise (<anonymous>)
+<e>   │     at AsyncSeriesHook.promiseStageRange (C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\@rspack\lite-tapable\dist\index.cjs:104:16)
+<e>   │     at QueriedHook.promise (C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\@rspack\lite-tapable\dist\index.cjs:210:26)
+<e>   │     at C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\@rspack\core\dist\index.js:11138:44
+<e>   │     at last.function (C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\@rspack\core\dist\index.js:11632:34)
+<e>   │
+<e> ] {
+<e>   code: 'GenericFailure'
+<e> }
