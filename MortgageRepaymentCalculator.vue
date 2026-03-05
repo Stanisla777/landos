3149995fea319a905d5cef25d5d54c17cce461b6
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2026-03-05 18:57:21 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,8 +1,6 @@
-× HTML parse error: Invalid first character of tag name
-    ╭─[43:49]
- 41 │                     .questions-and-answers__main
- 42 │                         ul.regulations-act
- 43 │                             - for (var x = 0; x < 10; x++)
-    ·                                                  ─
- 44 │                                 li.regulations-act__item
- 45 │                                     a(href="#").
+ul.regulations-act
+    - var n = 0
+    while n < 10
+        li.regulations-act__item
+            a(href="#")
+        - n++
