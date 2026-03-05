161fd6eb44a155328d666672b602035aeb118e3d
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2026-03-05 21:14:45 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,5 +1,19 @@
-p.personal-office__user-table-wr 25 000
-
-each val in [1,2,3,4,5,6,7,8,9,10]
-    .personal-office__table
-        .personal-office__table-item
+<html>
+    <head>
+        <link href="/css/instruction.css" rel="stylesheet" />
+    </head>
+    <body>
+        extends ../templates/base block title title= 'Инструкция' meta(name="description" content="Описание страницы")
+        block content .pages-wrap .pages-content.js--page-content include ../includes/header-personal-account +header(0)
+        include ../includes/blocks/redesign-main-page/sidebar main .container .block .page-tags
+        .page-tags__item.page-tags__item_time 2508.2023 a(href="#").page-tags__item #ипотека a(href="#").page-tags__item
+        #covid-19 a(href="#").page-tags__item #Оформление собственности .page-title-block
+        h1.page-title-block__headline.page-title-block__headline_little Как сэкономить до&nbsp;2&nbsp;млн&nbsp;рублей
+        на&nbsp;ипотеке? Скажите мне //.page-title-block__search-wrapper // include ../mixins/search-global //
+        +search-global .page-desc p Актуальные способы сэкономить на&nbsp;ипотеке
+        до&nbsp;2&nbsp;млн&nbsp;рублей&nbsp;в&nbsp;нашей инструкции. Игого include ../mixins/card-instruction
+        .white-back .container .block.block_content .block-with-aside .block-with-aside__main .content-wrapper
+        .content-block h2.content-title#title1 Какие меры господдержки позволяют сэкономить
+        до&nbsp;2&nbsp;млн&nbsp;рублей на ипотечном кредите? p В России действует целый ряд программ, позволяющих
+        молодым семьям с детьми значительно сэкономить при покупке жилья в ипотеку. Меры государственной поддержки могут
+        покрыть до 40% расходов по кредиту. //.content-block ul.bulleted-list.green li .........
