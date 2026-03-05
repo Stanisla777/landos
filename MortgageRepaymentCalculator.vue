ba42e667f254770d342f098d25a236a023ff4ffe
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2026-03-05 19:10:53 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,8 +1,5 @@
-HTML parse error: Invalid first character of tag name
-     ╭─[137:53]
- 135 │                                                 p.personal-office__user-table-wr 25 000
- 136 │ 
- 137 │                                         while count < 10
-     ·                                                      ─
- 138 │                                             .personal-office__table
- 139 │                                                 .personal-office__table-item
+p.personal-office__user-table-wr 25 000
+
+each val in [1,2,3,4,5,6,7,8,9,10]
+    .personal-office__table
+        .personal-office__table-item
