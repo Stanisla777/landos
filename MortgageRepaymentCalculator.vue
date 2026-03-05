52f7ec5f1ff4800e49553b6f8e76bbd4aa8ed747
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2026-03-05 21:56:24 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,20 +1,6 @@
-ERROR
-  × HTML parse error: Invalid first character of tag name
-     ╭─[176:65]
- 174 │                                     .swiper-container.js--swiper-authors
- 175 │                                         .swiper-wrapper
- 176 │                                             - for (var i = 1; i <= 4; i++)
-     ·                                                                  ─
- 177 │                                                 .swiper-slide
- 178 │                                                     a(href="#").authorship__item-author
+.pages-wrap
+    .pages-content.js--page-content
+        main
+            p Привет
      ╰────
-ERROR
-  × HTML parse error: Invalid first character of tag name
-     ╭─[238:49]
- 236 │                         include ../mixins/card-question
- 237 │                         .card-list.card-list_4-col.card-list_only-one-row
- 238 │                             - for (var i = 0; i < 4; i++)
-     ·                                                  ─
- 239 │                                 .card-list__col
- 240 │                                     +card-question(['Ипотека', 'Covid-19'], 'Что делать с ипотекой, если заболел или потерял доход из-за коронавируса?', 'card-question_white')
-     ╰────
+.pages-wrap .pages-content.js--page-content main p Привет
