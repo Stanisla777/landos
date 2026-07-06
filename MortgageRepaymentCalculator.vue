967c3f360c1916431bfc0dd93bb8131e8ecf8b53
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2026-07-06 10:26:10 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,17 +1,25 @@
-null-loader.js
-
-// null-loader.js
-module.exports = function() {
-  // Возвращаем пустой модуль
-  this.cacheable && this.cacheable();
-  return '';
-};
-
-{
-      test: /\.scss$/,
-      include: /@ringsites[\\/]ui-components[\\/]components/,
-      issuer: /(\?vue|\.vue\.(scss|css))/,
-      use: [
-        { loader: path.resolve(__dirname, 'null-loader.js') }
-      ]
-    },
+new rspack.IgnorePlugin({
+  checkResource(resource, context) {
+    // Убираем query string для проверки
+    const cleanResource = resource.split('?')[0];
+    
+    // Проверяем, что это SCSS/CSS файл из UI-кита
+    const isUiComponentStyle = /@ringsites[\\/]ui-components[\\/]components/.test(cleanResource) && 
+                                /\.(scss|css)$/.test(cleanResource);
+    
+    if (isUiComponentStyle) {
+      // Проверяем, что импорт идет из vue-loader
+      // context может содержать путь к .vue файлу или к виртуальному модулю vue-loader
+      const isFromVueLoader = /\.vue/.test(context) || 
+                              /vue-loader/.test(context) ||
+                              /\?vue/.test(context) ||
+                              /\.vue\.(scss|css)/.test(context);
+      
+      if (isFromVueLoader) {
+        console.log('[IgnorePlugin] Игнорируем стиль из UI-кита:', resource, 'из контекста:', context);
+        return true;
+      }
+    }
+    return false;
+  }
+})
