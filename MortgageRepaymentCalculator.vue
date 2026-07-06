f9426771a27c0bf76d5019829195412d02a01a47
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2026-07-06 13:05:02 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,20 +1,15 @@
 ignore-ui-styles-loader.js
 
-// ignore-ui-styles-loader.js
 module.exports = function(source) {
   this.cacheable && this.cacheable();
-  console.log('[ignore-ui-styles-loader] Обрабатываем файл:', this.resourcePath);
-  console.log('[ignore-ui-styles-loader] Контекст:', this.context);
   
-  // Возвращаем пустой модуль
-  return '';
+  // Логируем ВСЕ файлы, чтобы увидеть реальные пути
+  console.log('[LOADER] === SCSS файл ===');
+  console.log('[LOADER] resourcePath:', this.resourcePath);
+  console.log('[LOADER] context:', this.context);
+  console.log('[LOADER] issuer:', this._module?.issuer?.resource);
+  console.log('[LOADER] =================');
+  
+  // Возвращаем исходный код (пока не игнорируем)
+  return source;
 };
-
-
-{
-      test: /\.(scss|css)$/,
-      include: /@ringsites[\\/]ui-components[\\/]components/,
-      use: [
-        { loader: path.resolve(__dirname, 'ignore-ui-styles-loader.js') }
-      ]
-    },
