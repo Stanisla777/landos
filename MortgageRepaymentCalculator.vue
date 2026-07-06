334c3884fe1f736a619ba996065ca3c952373629
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2026-07-06 14:04:06 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,17 +1,1 @@
-null-loader.js
-
-// null-loader.js
-module.exports = function() {
-  this.cacheable && this.cacheable();
-  return '';
-};
-
-
- // 🎯 НОВОЕ: Игнорируем CSS из dist/ UI-кита
-    {
-      test: /\.css$/,
-      include: /@ringsites[\\/]ui-components[\\/]dist/,
-      use: [
-        { loader: path.resolve(__dirname, 'null-loader.js') }
-      ]
-    },
+npm install @ringsites/ui-components
