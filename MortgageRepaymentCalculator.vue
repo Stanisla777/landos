9d1c60a67f31fb89dc61cfb9ccf2557785e3d763
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2026-07-06 10:18:16 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,5 +1,12 @@
 null-loader.js
 
+// null-loader.js
+module.exports = function() {
+  // Возвращаем пустой модуль
+  this.cacheable && this.cacheable();
+  return '';
+};
+
 {
       test: /\.scss$/,
       include: /@ringsites[\\/]ui-components[\\/]components/,
