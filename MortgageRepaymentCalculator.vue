c1b9a0e1650269a0f14205aee9620b2249124037
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2026-07-06 13:15:18 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,15 +1,28 @@
-ignore-ui-styles-loader.js
-
-module.exports = function(source) {
-  this.cacheable && this.cacheable();
-  
-  // Логируем ВСЕ файлы, чтобы увидеть реальные пути
-  console.log('[LOADER] === SCSS файл ===');
-  console.log('[LOADER] resourcePath:', this.resourcePath);
-  console.log('[LOADER] context:', this.context);
-  console.log('[LOADER] issuer:', this._module?.issuer?.resource);
-  console.log('[LOADER] =================');
-  
-  // Возвращаем исходный код (пока не игнорируем)
-  return source;
-};
+[LOADER] === SCSS файл ===
+[LOADER] resourcePath: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\scss\general-style.scss
+[LOADER] context: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\scss
+[LOADER] issuer: undefined
+[LOADER] =================
+●  ━━━━━━━━━━━━━━━━━━━━━━━━━ (62%) building C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\babel-loader\lib\index.js??clonedRuleSet-6.use!C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\rspack-vue-
+[LOADER] === SCSS файл ===
+[LOADER] resourcePath: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\scss\style.scss
+[LOADER] context: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\scss
+[LOADER] issuer: undefined
+[LOADER] =================
+●  ━━━━━━━━━━━━━━━━━━━━━━━━━ (63%) building C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\babel-loader\lib\index.js??clonedRuleSet-6.use!C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\source-map-
+[LOADER] === SCSS файл ===
+[LOADER] resourcePath: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\scss\instruction.scss
+[LOADER] context: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\scss
+[LOADER] issuer: undefined
+[LOADER] =================
+●  ━━━━━━━━━━━━━━━━━━━━━━━━━ (64%) building C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\babel-loader\lib\index.js??clonedRuleSet-6.use!C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\rspack-vue-
+[LOADER] === SCSS файл ===
+[LOADER] resourcePath: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\@ringsites\ui-components\dist\DsButton.css
+[LOADER] context: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\@ringsites\ui-components\dist
+[LOADER] issuer: undefined
+[LOADER] =================
+[LOADER] === SCSS файл ===
+[LOADER] resourcePath: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\@ringsites\ui-components\dist\DsCheckbox.css
+[LOADER] context: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\@ringsites\ui-components\dist
+[LOADER] issuer: undefined
+[LOADER] =================
