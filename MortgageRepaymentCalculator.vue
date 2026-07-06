fc9c179d29d3cc4b5e38c1a76520f464af24d7ea
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2026-07-06 13:25:31 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,28 +1,17 @@
-[LOADER] === SCSS файл ===
-[LOADER] resourcePath: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\scss\general-style.scss
-[LOADER] context: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\scss
-[LOADER] issuer: undefined
-[LOADER] =================
-●  ━━━━━━━━━━━━━━━━━━━━━━━━━ (62%) building C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\babel-loader\lib\index.js??clonedRuleSet-6.use!C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\rspack-vue-
-[LOADER] === SCSS файл ===
-[LOADER] resourcePath: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\scss\style.scss
-[LOADER] context: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\scss
-[LOADER] issuer: undefined
-[LOADER] =================
-●  ━━━━━━━━━━━━━━━━━━━━━━━━━ (63%) building C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\babel-loader\lib\index.js??clonedRuleSet-6.use!C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\source-map-
-[LOADER] === SCSS файл ===
-[LOADER] resourcePath: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\scss\instruction.scss
-[LOADER] context: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\src\scss
-[LOADER] issuer: undefined
-[LOADER] =================
-●  ━━━━━━━━━━━━━━━━━━━━━━━━━ (64%) building C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\babel-loader\lib\index.js??clonedRuleSet-6.use!C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\rspack-vue-
-[LOADER] === SCSS файл ===
-[LOADER] resourcePath: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\@ringsites\ui-components\dist\DsButton.css
-[LOADER] context: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\@ringsites\ui-components\dist
-[LOADER] issuer: undefined
-[LOADER] =================
-[LOADER] === SCSS файл ===
-[LOADER] resourcePath: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\@ringsites\ui-components\dist\DsCheckbox.css
-[LOADER] context: C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf\node_modules\@ringsites\ui-components\dist
-[LOADER] issuer: undefined
-[LOADER] =================
+null-loader.js
+
+// null-loader.js
+module.exports = function() {
+  this.cacheable && this.cacheable();
+  return '';
+};
+
+
+ // 🎯 НОВОЕ: Игнорируем CSS из dist/ UI-кита
+    {
+      test: /\.css$/,
+      include: /@ringsites[\\/]ui-components[\\/]dist/,
+      use: [
+        { loader: path.resolve(__dirname, 'null-loader.js') }
+      ]
+    },
