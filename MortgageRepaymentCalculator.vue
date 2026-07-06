c59756c494d52c2712068bce47735a22da2e54cb
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2026-07-06 10:14:37 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,8 +1,13 @@
-<div class="fixed-bottom-widgets">
-      <div class="telegram__block" data-vue-app="telegram-block">
-            <article class="telegram">....</article>
-      </div>
-      <div class="cookies-block" data-vue-app="cookies-block">
-            <div class="cookies-vue"></div>
-      </div>
-</div>
+При установке пакета null-loader получил такую ошибку:
+C:\Users\sshchegolev\PhpstormProjects\sprosi.dom.rf>npm install --save-dev null-loader
+\(node:74212) Warning: Setting the NODE_TLS_REJECT_UNAUTHORIZED environment variable to '0' makes TLS connections and HTTPS requests insecure by disabling certificate verification.
+(Use `node --trace-warnings ...` to show where the warning was created)
+npm notice
+npm notice New major version of npm available! 10.7.0 -> 11.18.0
+npm notice Changelog: https://github.com/npm/cli/releases/tag/v11.18.0
+npm notice To update run: npm install -g npm@11.18.0
+npm notice
+npm error code ETARGET
+npm error notarget No matching version found for @ringsites/ui-components@^0.3.8.
+npm error notarget In most cases you or one of your dependencies are requesting
+npm error notarget a package version that doesn't exist.
