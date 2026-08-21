449320ce03232273a000c1a8e411754335346550
Stanisla777 <31473930+Stanisla777@users.noreply.github.com>
2026-08-21 12:16:05 +0300

    Update MortgageRepaymentCalculator.vue

--- MortgageRepaymentCalculator.vue
+++ MortgageRepaymentCalculator.vue
@@ -1,3 +1,30 @@
-sudo systemctl restart nginx
+<div class="test-n-slider" ref="ContainerSlider">
+        <Slider
+          ref="slider"
+          :modelValue="dragValue"
+          @update:modelValue="onValueChange"
+          @dragstart="isDragging = true"
+          @dragend="isDragging = false"
+          :min="0"
+          :max="optionsList.length - 1"
+          :step="0.01"
+          :tooltip="false"
+          :height="4"
+          :dot-size="24"
+          :disabled="time_end"
+          class="test-n-slider__track"
+        />
 
-Добрый день.  Не получается сменить пароль к учётной записи sshchegolev_adm для последующего подключения по ssh к domsite-d-app01. Пишет пароль неверный. Видимо проблема со старым паролем. Вы сможете помочь в решении проблемы?
+        <div class="test-n-slider__marks" :class="{ 'is-disabled': time_end }">
+          <div
+            v-for="(option, index) in optionsList"
+            :key="option.id"
+            class="test-n-slider__mark"
+            :class="{ 'is-active': index === selectedIndex }"
+            :style="{ left: getEvenPosition(index) + '%' }"
+            @click.stop="selectByIndex(index)"
+          >
+            <span class="test-n-slider__mark-label">{{ option.name }}</span>
+          </div>
+        </div>
+      </div>
